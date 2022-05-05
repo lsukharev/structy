27549c627946aee78a6b46dd555d674881d78a0f
Leo Sukharev <leosukharev@gmail.com>
2022-05-04 22:37:40 -0700

    Create a new Typescript project with Jest. Add solution for 002 (hey programmer).

---
+++ src/01-introduction/002-hey-programmer.ts
@@ -0,0 +1,13 @@
+/**
+ * hey programmer
+ *
+ * Welcome to Structy! Watch the approach video in the tab at the top of this
+ * window to get started.
+ *
+ * Write a function, greet, that takes in a string argument, s, and returns
+ * the string 'hey s'. No tricks here. Run the tests to check your work.
+ */
+
+export const greet = (s: string) => {
+  return 'hey ' + s;
+};
