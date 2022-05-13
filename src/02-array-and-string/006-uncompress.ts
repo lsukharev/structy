96e9c649d1f6f01744aeae8970bc918e2d6d74d8
Leo Sukharev <leosukharev@gmail.com>
2022-05-13 07:23:57 -0700

    Add solution for 012 (intersection)

--- src/02-array-and-string/006-uncompress.ts
+++ src/02-array-and-string/006-uncompress.ts
@@ -16,8 +16,8 @@
  *
  * - n = number of groups
  * - m = max num found in any group
- * - Time: O(n*m)
- * - Space: O(n*m)
+ * - Time: O(n * m)
+ * - Space: O(n * m)
  */
 
 export default function uncompress(str: string): string {
