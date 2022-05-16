82097a1d4e914fbf9719c000110c4a19146f3180
Leo Sukharev <leosukharev@gmail.com>
2022-05-16 07:20:46 -0700

    Add solution for 014 (five sort)

---
+++ src/02-array-and-string/014-five-sort.ts
@@ -0,0 +1,35 @@
+/**
+ * five sort
+ *
+ * Write a function, fiveSort, that takes in an array of numbers as an
+ * argument. The function should rearrange elements of the array such that all
+ * 5s appear at the end. Your function should perform this operation in-place
+ * by mutating the original array. The function should return the array.
+ *
+ * Elements that are not 5 can appear in any order in the output, as long as
+ * all 5s are at the end of the array.
+ *
+ * - n = array size
+ * - Time: O(n)
+ * - Space: O(1)
+ */
+
+export default function fiveSort(nums: number[]): number[] {
+  let i = 0;
+  let j = nums.length - 1;
+
+  while (i <= j) {
+    if (nums[j] === 5) {
+      j -= 1;
+      continue;
+    }
+
+    if (nums[i] === 5) {
+      [nums[i], nums[j]] = [nums[j], nums[i]];
+    }
+
+    i += 1;
+  }
+
+  return nums;
+}
