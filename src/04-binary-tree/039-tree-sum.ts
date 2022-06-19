18536410c5140831a509c5772a103fa02900059d
Leo Sukharev <leosukharev@gmail.com>
2022-06-19 09:18:32 -0700

    Add solution for 039 (tree sum)

---
+++ src/04-binary-tree/039-tree-sum.ts
@@ -0,0 +1,55 @@
+import Node from './Node';
+
+/**
+ * tree sum
+ *
+ * Write a function, treeSum, that takes in the root of a binary tree that
+ * contains number values. The function should return the total sum of all
+ * values in the tree.
+ *
+ * - n = number of nodes
+ * - Time: O(n)
+ * - Space: O(n)
+ */
+export function treeSum(root: Node<number>): number {
+  if (root === null) {
+    return 0;
+  }
+
+  const stack = [root];
+  let sum = 0;
+
+  while (stack.length > 0) {
+    const curr = stack.pop();
+    sum += curr.data;
+
+    if (curr.right !== null) {
+      stack.push(curr.right);
+    }
+
+    if (curr.left !== null) {
+      stack.push(curr.left);
+    }
+  }
+
+  return sum;
+}
+
+/**
+ * tree sum (recursive)
+ *
+ * Write a recursive function, treeSumRecursive, that takes in the root of a
+ * binary tree that contains number values. The function should return the
+ * total sum of all values in the tree.
+ *
+ * - n = number of nodes
+ * - Time: O(n)
+ * - Space: O(n)
+ */
+export function treeSumRecursive(root: Node<number>): number {
+  if (root === null) {
+    return 0;
+  }
+
+  return root.data + treeSumRecursive(root.left) + treeSumRecursive(root.right);
+}
---
+++ test/04-binary-tree/039-tree-sum.test.ts
@@ -0,0 +1,76 @@
+import Node from '../../src/04-binary-tree/Node';
+import { treeSum, treeSumRecursive } from '../../src/04-binary-tree/039-tree-sum';
+
+describe('test_00', () => {
+  const a = new Node(3);
+  const b = new Node(11);
+  const c = new Node(4);
+  const d = new Node(4);
+  const e = new Node(-2);
+  const f = new Node(1);
+
+  a.left = b;
+  a.right = c;
+  b.left = d;
+  b.right = e;
+  c.right = f;
+
+  //       3
+  //    /    \
+  //   11     4
+  //  / \      \
+  // 4   -2     1
+
+  test('iterative', () => {
+    expect(treeSum(a)).toBe(21);
+  });
+
+  test('recursive', () => {
+    expect(treeSumRecursive(a)).toBe(21);
+  });
+});
+
+describe('test_01', () => {
+  const a = new Node(1);
+  const b = new Node(6);
+  const c = new Node(0);
+  const d = new Node(3);
+  const e = new Node(-6);
+  const f = new Node(2);
+  const g = new Node(2);
+  const h = new Node(2);
+
+  a.left = b;
+  a.right = c;
+  b.left = d;
+  b.right = e;
+  c.right = f;
+  e.left = g;
+  f.right = h;
+
+  //      1
+  //    /   \
+  //   6     0
+  //  / \     \
+  // 3   -6    2
+  //    /       \
+  //   2         2
+
+  test('iterative', () => {
+    expect(treeSum(a)).toBe(10);
+  });
+
+  test('recursive', () => {
+    expect(treeSumRecursive(a)).toBe(10);
+  });
+});
+
+describe('test_02', () => {
+  test('iterative', () => {
+    expect(treeSum(null)).toBe(0);
+  });
+
+  test('recursive', () => {
+    expect(treeSumRecursive(null)).toBe(0);
+  });
+});
