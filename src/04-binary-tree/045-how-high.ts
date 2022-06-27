fd10a886bdd8b0d8512faf4d5a5068134aa78995
Leo Sukharev <leosukharev@gmail.com>
2022-06-27 07:56:40 -0700

    Add solution for 045 (how high)

---
+++ src/04-binary-tree/045-how-high.ts
@@ -0,0 +1,24 @@
+import Node from './Node';
+
+/**
+ * how high
+ *
+ * Write a function, howHigh, that takes in the root of a binary tree. The
+ * function should return a number representing the height of the tree.
+ *
+ * The height of a binary tree is defined as the maximal number of edges from
+ * the root node to any leaf node.
+ *
+ * If the tree is empty, return -1.
+ *
+ * - n = number of nodes
+ * - Time: O(n)
+ * - Space: O(n)
+ */
+export default function howHigh(root: Node<any> | null): number {
+  if (root === null) {
+    return -1;
+  }
+
+  return 1 + Math.max(howHigh(root.left), howHigh(root.right));
+}
