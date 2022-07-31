7886129c6660316790a20007c3c85d7603af581b
Leo Sukharev <leosukharev@gmail.com>
2022-07-30 19:12:43 -0700

    Add solution for 059 (largest component)

---
+++ src/05-graph/059-largest-component.ts
@@ -0,0 +1,44 @@
+import { AdjacencyList } from "./common";
+
+/**
+ * largest component
+ *
+ * Write a function, largestComponent, that takes in the adjacency list of an
+ * undirected graph. The function should return the size of the largest
+ * connected component in the graph.
+ *
+ * - n = number of nodes
+ * - e = number of edges
+ * - Time: O(e)
+ * - Space: O(n)
+ */
+export default function largestComponent<T>(graph: AdjacencyList<T>): number {
+  const visited = new Set<string>();
+  let largestSize = 0;
+
+  for (let node in graph) {
+    const currentSize = explore(graph, node, visited);
+
+    if (currentSize > largestSize) {
+      largestSize = currentSize;
+    }
+  }
+
+  return largestSize;
+}
+
+function explore<T>(graph: AdjacencyList<T>, node: string, visited: Set<string>): number {
+  if (visited.has(node)) {
+    return 0;
+  }
+
+  visited.add(node);
+
+  let size = 1;
+
+  for (let neighbor of graph[node]) {
+    size += explore(graph, neighbor.toString(), visited);
+  }
+
+  return size;
+}
