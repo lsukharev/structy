8fe8420ac406aef5907b77411f6acad85a82189a
Leo Sukharev <leosukharev@gmail.com>
2022-09-19 08:12:23 -0700

    Add solution for 096 (permutations)

---
+++ src/08-exhaustive-recursion/096-permutations.ts
@@ -0,0 +1,31 @@
+/**
+ * permutations
+ *
+ * Write a function, permutations, that takes in an array an argument. The
+ * function should return a 2D array where each subarray represents one of the
+ * possible permutations of the array.
+ *
+ * The subarrays may be returned in any order.
+ *
+ * You may assume that the input array contains unique elements.
+ *
+ * - n = length of elements array
+ * - Time: ~O(n!)
+ * - Space: ~O(n!)
+ */
+export default function permutations<T>(elements: T[]): T[][] {
+  if (elements.length === 0) {
+    return [[]];
+  }
+
+  const elem = elements.shift();
+  const result: T[][] = [];
+
+  for (const permuation of permutations(elements)) {
+    for (let index = 0; index <= permuation.length; index += 1) {
+      result.push([...permuation.slice(0, index), elem, ...permuation.slice(index)]);
+    }
+  }
+
+  return result;
+}
