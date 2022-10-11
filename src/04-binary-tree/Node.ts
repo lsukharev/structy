8538a59490f8bbcbb98ccb7ec38650ea1210ccff
Leo Sukharev <leosukharev@gmail.com>
2022-10-11 08:37:53 -0700

    Add solution for 106 (flip tree)

--- src/04-binary-tree/Node.ts
+++ src/04-binary-tree/Node.ts
@@ -8,4 +8,25 @@ export default class Node<T> {
     this.left = null;
     this.right = null;
   }
+
+  toString(): string {
+    let str = '';
+    const queue: Node<any>[] = [this];
+
+    while (queue.length > 0) {
+      const current = queue.shift();
+
+      str += current.data.toString();
+
+      if (current.left !== null) {
+        queue.push(current.left);
+      }
+
+      if (current.right !== null) {
+        queue.push(current.right);
+      }
+    }
+
+    return str;
+  }
 }
