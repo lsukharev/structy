c416ddbff5e1427795dd51405d53b9939b1418a2
Leo Sukharev <leosukharev@gmail.com>
2022-10-23 15:35:44 -0700

    Add solution for 110 (rare routing)

---
+++ test/09-mixed-recall/110-rare-routing.test.ts
@@ -0,0 +1,56 @@
+import rareRouting from '../../src/09-mixed-recall/110-rare-routing';
+
+test('test_00', () => {
+  const roads: [number, number][] = [
+    [0, 1],
+    [0, 2],
+    [0, 3]
+  ];
+
+  expect(rareRouting(4, roads)).toBe(true);
+});
+
+test('test_01', () => {
+  const roads: [number, number][] = [
+    [0, 1],
+    [0, 2],
+    [0, 3],
+    [3, 2]
+  ];
+
+  expect(rareRouting(4, roads)).toBe(false);
+});
+
+test('test_02', () => {
+  const roads: [number, number][] = [
+    [1, 2],
+    [5, 4],
+    [3, 0],
+    [0, 1],
+    [0, 4]
+  ];
+
+  expect(rareRouting(6, roads)).toBe(true);
+});
+
+test('test_03', () => {
+  const roads: [number, number][] = [
+    [1, 2],
+    [4, 1],
+    [5, 4],
+    [3, 0],
+    [0, 1],
+    [0, 4]
+  ];
+
+  expect(rareRouting(6, roads)).toBe(false);
+});
+
+test('test_04', () => {
+  const roads: [number, number][] = [
+    [0, 1],
+    [3, 2]
+  ];
+
+  expect(rareRouting(4, roads)).toBe(false);
+});
